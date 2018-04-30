5176b37d91a9491baf5421ed66bfa75f04031ec6
elzup <guild0105@gmail.com>
2018-04-30 23:20:49 +0900

    Fix to override npm list

--- backup-brew.sh
+++ backup-brew.sh
@@ -7,7 +7,7 @@ repository=$HOME/Brewfile
 
 cd "$repository"
 /usr/local/bin/brew bundle dump -f
-/usr/local/bin/npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' >> npm-global-list
+/usr/local/bin/npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' > npm-global-list
 /usr/bin/git add .
 /usr/bin/git commit -m "Backup Brewfile `date "+%Y-%m-%d %H:%M:%S"`"
 /usr/bin/git push origin master

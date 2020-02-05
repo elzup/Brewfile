eb690e2c127958b3b616c7550f4f37fc81c1dc22
elzup <guild0105@gmail.com>
2020-02-05 13:40:29 +0900

    add morning newspaper

---
+++ morning-newspaper/morning-newspaper.sh
@@ -0,0 +1,4 @@
+open "https://github.com/trending/javascript?since=daily"
+open "https://github.com/trending/typescript?since=daily"
+open "https://twitter.com/javascriptdaily"
+open "https://twitter.com/i/lists/1072318038316244992"

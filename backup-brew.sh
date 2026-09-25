0c78b3c8c6f9b3c683365c2dcfdc792b9cdb3cbc
elzup <guild0105@gmail.com>
2026-09-26 06:01:34 +0900

    Backup Brewfile 2026-09-26 06:01:34

--- backup-brew.sh
+++ backup-brew.sh
@@ -11,6 +11,9 @@ rm Brewfile
 /opt/homebrew/bin/brew bundle dump -f
 /usr/local/bin/npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' > npm-global-list
 /usr/bin/git add .
-/usr/bin/git commit -m "Backup Brewfile `date "+%Y-%m-%d %H:%M:%S"`"
-/usr/bin/git push origin master
+# 変更の無い日に commit が exit 1 になり、set -e でジョブ全体が失敗扱いになるのを避ける。
+# push は毎回行い、過去に失敗して溜まった未 push 分も送る
+/usr/bin/git diff --cached --quiet || /usr/bin/git commit -m "Backup Brewfile `date "+%Y-%m-%d %H:%M:%S"`"
+# refspec を明示する。ブランチ名を master から main に変えた後も master へ push していて毎日失敗していた
+/usr/bin/git push origin HEAD:refs/heads/main
 osascript -e 'display notification "Finish backing up  Brewfile" with title "Brewfile"'
